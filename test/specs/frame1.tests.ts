552876ff91068c832c957599cbe4529464dc82f3
harshwardhan <harshwsinha08@gmail.com>
2025-04-30 11:18:18 +0530

    typescriptInstall3

---
+++ test/specs/frame1.tests.ts
@@ -0,0 +1,134 @@
+// /test/specs/frame1.test.ts
+import framePage from '../pageobjects/frame.page.js';
+
+
+describe('Frame Testing with WebDriverIO', () => {
+    beforeEach(async () => {
+        await framePage.open();
+    });
+
+    it('C24 test17', async () => {
+            await framePage.normalTest();
+        });
+    
+        it('C25 test18', async () => {
+            await framePage.normalTest();
+        });
+    
+        it('C26 test19', async () => {
+            await framePage.normalTest();
+        });
+    
+        it('C27 test20', async () => {
+            await framePage.normalTest();
+        });
+    
+        it('C28 test21', async () => {
+            await framePage.normalTest();
+        });
+    
+        it('C29 test22', async () => {
+            await framePage.normalTest();
+        });
+    
+        it('C30 test23', async () => {
+            await framePage.normalTest();
+        });
+    
+        it('C31 test24', async () => {
+            await framePage.normalTest();
+        });
+    
+        it('C32 test25', async () => {
+            await framePage.normalTest();
+        });
+    
+        it('C33 test26', async () => {
+            await framePage.normalTest();
+        });
+    
+        it('C34 test27', async () => {
+            await framePage.normalTest();
+        });
+    
+        it('C35 test28', async () => {
+            await framePage.normalTest();
+        });
+    
+        it('C36 test29', async () => {
+            await framePage.normalTest();
+        });
+    
+        it('C37 test30', async () => {
+            await framePage.normalTest();
+        });
+    
+        it('C38 test31', async () => {
+            await framePage.normalTest();
+        });
+    
+        it('C39 test32', async () => {
+            await framePage.normalTest();
+        });
+    
+        it('C40 test33', async () => {
+            await framePage.normalTest();
+        });
+    
+        it('C41 test34', async () => {
+            await framePage.normalTest();
+        });
+    
+        it('C42 test35', async () => {
+            await framePage.normalTest();
+        });
+    
+        it('C43 test36', async () => {
+            await framePage.normalTest();
+        });
+    
+        it('C44 test37', async () => {
+            await framePage.normalTest();
+        });
+    
+        it('C45 test38', async () => {
+            await framePage.normalTest();
+        });
+    
+        it('C46 test39', async () => {
+            await framePage.normalTest();
+        });
+    
+        it('C47 test40', async () => {
+            await framePage.normalTest();
+        });
+    
+        it('C48 test41', async () => {
+            await framePage.normalTest();
+        });
+    
+        it('C49 test42', async () => {
+            await framePage.normalTest();
+        });
+    
+        it('C50 test43', async () => {
+            await framePage.normalTest();
+        });
+    
+        it('C51 test44', async () => {
+            await framePage.normalTest();
+        });
+    
+        it('C52 test45', async () => {
+            await framePage.normalTest();
+        });
+    
+        it('C53 test46', async () => {
+            await framePage.normalTest();
+        });
+    
+        it('C54 test47', async () => {
+            await framePage.normalTest();
+        });
+
+});
