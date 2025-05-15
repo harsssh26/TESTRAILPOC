f41240886f45968959a4a059b76c623825b4d36c
harshwardhan <harshwsinha08@gmail.com>
2025-05-15 12:19:37 +0530

    RecentChanges

--- wdio.conf.ts
+++ wdio.conf.ts
@@ -1,4 +1,4 @@
-// /wdio.conf.ts
+// wdio.conf.ts
 import { config as dotenvConfig } from 'dotenv';
 import type { Options } from '@wdio/types';
 
@@ -12,64 +12,67 @@ const testRailProjectId = process.env.TESTRAIL_PROJECT_ID;
 const testRailSuiteId = process.env.TESTRAIL_SUITE_ID;
 
 const reporters: Options.Testrunner['reporters'] = [
-    'spec',
-    ['allure', {
-      outputDir: 'allure-results',
-      disableWebdriverStepsReporting: true,
-      disableWebdriverScreenshotsReporting: false
-    }]
-  ];
+  'spec',
+  ['allure', {
+    outputDir: 'allure-results',
+    disableWebdriverStepsReporting: true,
+    disableWebdriverScreenshotsReporting: false,
+    addConsoleLogs: true,
+  }]
+];
 
-  export const config: WebdriverIO.Config = {
-    runner: 'local',
-    autoCompile: true, 
+if (isScheduled && testRailDomain && testRailUserName && testRailApiToken) {
+  const WdioTestRailReporter = require('./wdio-testrail-reporter');
+  reporters.push([
+    WdioTestRailReporter,
+    {
+      testRailsOptions: {
+        domain: testRailDomain,
+        username: testRailUserName,
+        apiToken: testRailApiToken,
+        projectId: parseInt(testRailProjectId!),
+        suiteId: parseInt(testRailSuiteId!)
+      },
+      runName: `Automation Run - ${new Date().toLocaleString('en-IN', { timeZone: 'Asia/Kolkata' })}`,
+      includeAll: false,
+      oneReport: true
+    }
+  ]);
+}
+
+export const config: WebdriverIO.Config = {
+  runner: 'local',
+  autoCompileOpts: {
+    autoCompile: true,
     tsNodeOpts: {
       project: './tsconfig.json',
       transpileOnly: true
-    }as any ,
-    specs: ['./test/specs/**/*.ts'],
-    maxInstances: 1,
-    capabilities: [{
-      browserName: 'chrome',
-      'goog:chromeOptions': {
-        args: ['--headless', '--disable-gpu']
-      }
-    }],
-    logLevel: 'info',
-    baseUrl: 'https://ui.vision/demo/webtest/frames/',
-    waitforTimeout: 10000,
-    connectionRetryTimeout: 120000,
-    connectionRetryCount: 3,
-    services: ['chromedriver'],
-    framework: 'mocha',
-    reporters,
-    mochaOpts: {
-      ui: 'bdd',
-      timeout: 60000
-    },
-    onPrepare: async function () {
-      if (isScheduled && testRailDomain && testRailUserName && testRailApiToken) {
-        const WdioTestRailReporter = require('./wdio-testrail-reporter');
-  
-        config.reporters!.push([
-          WdioTestRailReporter,
-          {
-            testRailsOptions: {
-              domain: testRailDomain,
-              username: testRailUserName,
-              apiToken: testRailApiToken,
-              projectId: parseInt(testRailProjectId!),
-              suiteId: parseInt(testRailSuiteId!)
-            },
-            runName: `Automation Run - ${new Date().toLocaleString('en-IN', { timeZone: 'Asia/Kolkata' })}`,
-            includeAll: false,
-            oneReport: true
-          }
-        ]);
-      }
-    },
-    before: async () => {
-      await browser.setWindowSize(1920, 1080);
     }
-  };
-  ;
+  },
+  specs: [
+    './test/specs/**/*.ts'
+  ],
+  exclude: [],
+  maxInstances: 1,
+  capabilities: [{
+    browserName: 'chrome',
+    'goog:chromeOptions': {
+      args: ['--headless', '--disable-gpu']
+    }
+  }],
+  logLevel: 'info',
+  baseUrl: 'https://ui.vision/demo/webtest/frames/',
+  waitforTimeout: 10000,
+  connectionRetryTimeout: 120000,
+  connectionRetryCount: 3,
+  services: ['chromedriver'],
+  framework: 'mocha',
+  reporters,
+  mochaOpts: {
+    ui: 'bdd',
+    timeout: 60000
+  },
+  before: async () => {
+    await browser.setWindowSize(1920, 1080);
+  }
+};
