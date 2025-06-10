4d347539c68e48e4db311a60daafe3d7bf68f823
harshwardhan <harshwsinha08@gmail.com>
2025-06-10 17:06:50 +0530

    latestChange5

--- test/specs/frame.test.ts
+++ test/specs/frame.test.ts
@@ -6,87 +6,87 @@ describe('Frame Testing with WebDriverIO', () => {
         await framePage.open();
     });
 
-    it('[FRAME] [DAILY] C3 should type into frame 1', async () => {
+    it('[FRAME] C3 should type into frame 1', async () => {
         await framePage.typeInFrame1('Hello');
     });
 
-    it('[FRAME] [DAILY] C4 should click checkbox in frame 3', async () => {
+    it('[FRAME] C4 should click checkbox in frame 3', async () => {
         await framePage.clickInFrame3();
     });
 
-    it('[FRAME] [DAILY] C5 should type into frame 2', async () => {
+    it('[FRAME] C5 should type into frame 2', async () => {
         await framePage.typeInFrame2('Hello');
     });
 
-    it('[AFRAME] [DAILY] C6 should type into frame 5 and click the link', async () => {
+    it('[FRAME] C6 should type into frame 5 and click the link', async () => {
         await framePage.normalTest();
     });
 
-    it('[AFRAME] [DAILY] C7 test0', async () => {
+    it('[FRAME] C7 test0', async () => {
         await framePage.normalTest();
     });
 
-    it('[FRAME] [DAILY] C8 test1', async () => {
+    it('[FRAME] C8 test1', async () => {
         await framePage.normalTest();
     });
 
-    it('[FRAME] [DAILY] C9 test2', async () => {
+    it('[FRAME] C9 test2', async () => {
         await framePage.normalTest();
     });
 
-    it('[FRAME] [DAILY] C10 test3', async () => {
+    it('[FRAME] C10 test3', async () => {
         await framePage.normalTest();
     });
 
-    it('[FRAME] [DAILY] C11 test4', async () => {
+    it('[FRAME] C11 test4', async () => {
         await framePage.normalTest();
     });
 
-    it('[FRAME] [WEEKLY] C12 test5', async () => {
+    it('[FRAME] C12 test5', async () => {
         await framePage.normalTest();
     });
 
-    it('[FRAME] [WEEKLY] C13 test6', async () => {
+    it('[FRAME] C13 test6', async () => {
         await framePage.normalTest();
     });
 
-    it('[FRAME] [WEEKLY] C14 test7', async () => {
+    it('[FRAME] C14 test7', async () => {
         await framePage.normalTest();
     });
 
-    it('[FRAME] [WEEKLY] C15 test8', async () => {
+    it('[FRAME] C15 test8', async () => {
         await framePage.normalTest();
     });
 
-    it('[FRAME] [WEEKLY] C16 test9', async () => {
+    it('[FRAME] C16 test9', async () => {
         await framePage.normalTest();
     });
 
-    it('[FRAME] [WEEKLY] C17 test10', async () => {
+    it('[FRAME] C17 test10', async () => {
         await framePage.normalTest();
     });
 
-    it('[FRAME] [WEEKLY] C18 test11', async () => {
+    it('[FRAME] C18 test11', async () => {
         await framePage.normalTest();
     });
 
-    it('[FRAME] [WEEKLY] C19 test12', async () => {
+    it('[FRAME] C19 test12', async () => {
         await framePage.normalTest();
     });
 
-    it('[FRAME] [WEEKLY] C20 test13', async () => {
+    it('[FRAME] C20 test13', async () => {
         await framePage.normalTest();
     });
 
-    it('[FRAME] [WEEKLY] C21 test14', async () => {
+    it('[FRAME] C21 test14', async () => {
         await framePage.normalTest();
     });
 
-    it('[FRAME] [WEEKLY] C22 test15', async () => {
+    it('[FRAME] C22 test15', async () => {
         await framePage.normalTest();
     });
 
-    it('[FRAME] [WEEKLY] C23 test16', async () => {
+    it('[FRAME] C23 test16', async () => {
         await framePage.normalTest();
     });    
 
--- test/specs/frame2.test.ts
+++ test/specs/frame2.test.ts
@@ -7,63 +7,63 @@ describe('Frame Testing with WebDriverIO', () => {
         await framePage.open();
     });
 
-    it('[FRAME2] [DAILY] C55 test48', async () => {
+    it('[FRAME2] C55 test48', async () => {
         await framePage.normalTest();
     });
 
-    it('[FRAME2] [DAILY] C56 test49', async () => {
+    it('[FRAME2] C56 test49', async () => {
         await framePage.normalTest();
     });
 
-    it('[FRAME2] [DAILY] C57 test50', async () => {
+    it('[FRAME2] C57 test50', async () => {
         await framePage.normalTest();
     });
 
-    it('[FRAME2] [DAILY] C58 test51', async () => {
+    it('[FRAME2] C58 test51', async () => {
         await framePage.normalTest();
     });
 
-    it('[FRAME2] [DAILY] C59 test52', async () => {
+    it('[FRAME2] C59 test52', async () => {
         await framePage.normalTest();
     });
 
-    it('[FRAME2] [DAILY] C60 test53', async () => {
+    it('[FRAME2] C60 test53', async () => {
         await framePage.normalTest();
     });
 
-    it('[FRAME2] [DAILY] C61 test54', async () => {
+    it('[FRAME2] C61 test54', async () => {
         await framePage.normalTest();
     });
 
-    it('[FRAME2] [WEEKLY] C62 test55', async () => {
+    it('[FRAME2] C62 test55', async () => {
         await framePage.normalTest();
     });
 
-    it('[FRAME2] [WEEKLY] C63 test56', async () => {
+    it('[FRAME2] C63 test56', async () => {
         await framePage.normalTest();
     });
 
-    it('[FRAME2] [WEEKLY] C64 test57', async () => {
+    it('[FRAME2] C64 test57', async () => {
         await framePage.normalTest();
     });
 
-    it('[FRAME2] [WEEKLY] C65 test58', async () => {
+    it('[FRAME2] C65 test58', async () => {
         await framePage.normalTest();
     });
 
-    it('[FRAME2] [WEEKLY] C66 test59', async () => {
+    it('[FRAME2] C66 test59', async () => {
         await framePage.normalTest();
     });
 
-    it('[FRAME2] [WEEKLY] C67 test60', async () => {
+    it('[FRAME2] C67 test60', async () => {
         await framePage.normalTest();
     });
 
-    it('[FRAME2] [WEEKLY] C68 test61', async () => {
+    it('[FRAME2] C68 test61', async () => {
         await framePage.normalTest();
     });
 
-    it('[FRAME2] [WEEKLY] C69 test62', async () => {
+    it('[FRAME2] C69 test62', async () => {
         await framePage.normalTest();
     });
 
