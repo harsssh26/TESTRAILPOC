9f7d95c0ad456397892fdb8b0ad5bda2a071dbb2
harshwardhan <harshwsinha08@gmail.com>
2025-06-23 16:18:25 +0530

    cc

--- test/specs/frame.test.ts
+++ test/specs/frame.test.ts
@@ -1,76 +1,76 @@
 // /test/specs/frame.test.ts
 import framePage from '../pageobjects/frame.page.js';
 import fs from 'fs'
-describe('Frame Testing with WebDriverIO', () => {
+describe('[FRAME] Frame Testing with WebDriverIO', () => {
     beforeEach(async () => {
         await framePage.open();
     });
 
-    it('[FRAME] C3 should type into frame 1', async () => {
+    it('C3 should type into frame 1', async () => {
         await framePage.typeInFrame1('Hello');
     });
 
-    it('[FRAME] C4 should click checkbox in frame 3', async () => {
+    it('C4 should click checkbox in frame 3', async () => {
         await framePage.clickInFrame3();
     });
 
-    it('[FRAME] C5 should type into frame 2', async () => {
+    it('C5 should type into frame 2', async () => {
         await framePage.typeInFrame2('Hello');
     });
 
-    it('[FRAME] C6 should type into frame 5 and click the link', async () => {
+    it('C6 should type into frame 5 and click the link', async () => {
         await framePage.normalTest();
     });
 
-    it('[FRAME] C7 test0', async () => {
+    it('C7 test0', async () => {
         await framePage.normalTest();
     });
 
-    it('[FRAME] C8 test1', async () => {
+    it('C8 test1', async () => {
         await framePage.normalTest();
     });
 
-    it('[FRAME] C9 test2', async () => {
+    it('C9 test2', async () => {
         await framePage.normalTest();
     });
 
-    it('[FRAME] C10 test3', async () => {
+    it('C10 test3', async () => {
         await framePage.normalTest();
     });
 
-    it('[FRAME] C11 test4', async () => {
+    it('C11 test4', async () => {
         await framePage.normalTest();
     });
 
-    it('[FRAME] C12 test5', async () => {
+    it('C12 test5', async () => {
         await framePage.normalTest();
     });
 
-    it('[FRAME] C13 test6', async () => {
+    it('C13 test6', async () => {
         await framePage.normalTest();
     });
 
-    it('[FRAME] C14 test7', async () => {
+    it('C14 test7', async () => {
         await framePage.normalTest();
     });
 
-    it('[FRAME] C15 test8', async () => {
+    it('C15 test8', async () => {
         await framePage.normalTest();
     });
 
-    it('[FRAME] C16 test9', async () => {
+    it('C16 test9', async () => {
         await framePage.normalTest();
     });
 
-    it('[FRAME] C17 test10', async () => {
+    it('C17 test10', async () => {
         await framePage.normalTest();
     });
 
-    it('[FRAME] C18 test11', async () => {
+    it('C18 test11', async () => {
         await framePage.normalTest();
     });
 
-    it('[FRAME] C19 test12', async () => {
+    it('C19 test12', async () => {
         await framePage.normalTest();
           const frameValue = 'FRAME_GENERATED_VALUE';
           const data = { sharedKey: frameValue };
@@ -78,19 +78,19 @@ describe('Frame Testing with WebDriverIO', () => {
           console.log('Written to shared-data.json:', data);
     });
 
-    it('[FRAME] C20 test13', async () => {
+    it('C20 test13', async () => {
         await framePage.normalTest();
     });
 
-    it('[FRAME] C21 test14', async () => {
+    it('C21 test14', async () => {
         await framePage.normalTest();
     });
 
-    it('[FRAME] C22 test15', async () => {
+    it('C22 test15', async () => {
         await framePage.normalTest();
     });
 
-    it('[FRAME] C23 test16', async () => {
+    it('C23 test16', async () => {
         await framePage.normalTest();
     });    
 
